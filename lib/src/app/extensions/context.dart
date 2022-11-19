1ff84d1bffd28e836616efb28df0043c1ef82f7a
Igor Guerra <igguerra07@gmail.com>
2022-11-19 16:03:50 -0400

    :art: add device size extensions

--- lib/src/app/extensions/context.dart
+++ lib/src/app/extensions/context.dart
@@ -4,4 +4,6 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 extension ContextExtensions on BuildContext {
   AppLocalizations? get l10n => AppLocalizations.of(this);
+  double get deviceHeight => MediaQuery.of(this).size.height;
+  double get deviceWidth => MediaQuery.of(this).size.width;
 }
