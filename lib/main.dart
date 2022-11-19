4fa5ee0ebf1edfdf1c888925590eb73791b6fd45
Igor Guerra <igguerra07@gmail.com>
2022-11-19 14:50:14 -0400

    :art: init getIt dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:newsapp/src/app/di/injector.dart';
 import 'package:newsapp/src/app/news_app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await configureDependencies();
   runApp(const NewsApp());
 }
-
