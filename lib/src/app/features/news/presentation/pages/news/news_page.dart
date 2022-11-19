2bcf88cd2af0f29268d1258e433da45514819e3a
Igor Guerra <igguerra07@gmail.com>
2022-11-19 14:52:09 -0400

    :art: get news store from di

--- lib/src/app/features/news/presentation/pages/news/news_page.dart
+++ lib/src/app/features/news/presentation/pages/news/news_page.dart
@@ -1,14 +1,10 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:newsapp/src/app/di/injector.dart';
 import 'package:newsapp/src/app/extensions/context.dart';
-import 'package:newsapp/src/app/features/news/data/datasources/remote/news_remote_data_impl.dart';
-import 'package:newsapp/src/app/features/news/data/repositories/news_repository_impl.dart';
-import 'package:newsapp/src/app/features/news/domain/usecases/get_treading_news.dart';
 import 'package:newsapp/src/app/features/news/presentation/pages/news/store/news_state.dart';
 import 'package:newsapp/src/app/features/news/presentation/pages/news/store/news_store.dart';
 import 'package:newsapp/src/app/features/news/presentation/pages/news/widgets/news_item.dart';
-import 'package:newsapp/src/infra/network/dio/dio_client.dart';
 
 class NewsPage extends StatefulWidget {
   const NewsPage({Key? key}) : super(key: key);
@@ -18,19 +14,11 @@ class NewsPage extends StatefulWidget {
 }
 
 class _NewsPageState extends State<NewsPage> {
-  late final NewsStore _newsStore;
+  final NewsStore _newsStore = NewsStore(getTreadingNews: getIt());
 
   @override
   void initState() {
-    final dio = Dio();
-    final apiClient = DioClient(dio: dio, baseUrl: "https://newsapi.org/v2/");
-    final remote = NewsRemoteDataSourceImpl(apiClient: apiClient);
-    final repository = NewsRepositoryImpl(remote: remote);
-    final getTreadingNews = GetTreadingNews(newsRepository: repository);
-    _newsStore = NewsStore(getTreadingNews: getTreadingNews);
-
     super.initState();
-
     _newsStore.getTreadingNews();
   }
 
@@ -41,7 +29,7 @@ class _NewsPageState extends State<NewsPage> {
         title: Text(context.l10n?.appName ?? ""),
         elevation: 0,
       ),
-      body: BlocBuilder( 
+      body: BlocBuilder(
         bloc: _newsStore,
         builder: (context, state) {
           if (state is NewsLoadingState) {
@@ -55,11 +43,14 @@ class _NewsPageState extends State<NewsPage> {
             );
           }
           if (state is NewsLoadedState) {
-            return ListView.separated(
-              padding: const EdgeInsets.all(16),
-              itemCount: state.news.length,
-              itemBuilder: (_, index) => NewsItem(news: state.news[index]),
-              separatorBuilder: (_, __) => const SizedBox(height: 16),
+            return RefreshIndicator(
+              onRefresh: () => _newsStore.getTreadingNews(),
+              child: ListView.separated(
+                padding: const EdgeInsets.all(16),
+                itemCount: state.news.length,
+                itemBuilder: (_, index) => NewsItem(news: state.news[index]),
+                separatorBuilder: (_, __) => const SizedBox(height: 16),
+              ),
             );
           }
           return const SizedBox.shrink();
